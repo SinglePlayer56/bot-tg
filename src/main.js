058bf2eea52774628da6e53e44fd199ab055d5ff
Viktor Korchagin <viktor.korchagin@kingmidasmedia.ru>
2023-04-30 23:28:02 +0500

    add text response

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ bot.on(message('voice'), async (ctx) => {
         const userId = String(ctx.message.from.id);
         const oggPath = await ogg.create(link.href, userId);
         const mp3Path = await ogg.toMp3(oggPath, userId);
-        // const text = ctx.update.message.text;
+
         const text = await openai.transcription(mp3Path);
         await ctx.reply(code(`Ваш запрос: ${text}`));
 
@@ -26,6 +26,23 @@ bot.on(message('voice'), async (ctx) => {
         console.log('Error while voice message', e.message);
     }
 });
+
+bot.on(message('text'), async (ctx) => {
+    try {
+        await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
+        const text = ctx.update.message.text;
+
+        await ctx.reply(code(`Ваш запрос: ${text}`));
+
+
+        const messages = [{role: openai.roles.USER, content: text}];
+        const response = await openai.chat(messages);
+
+        await ctx.reply(response.content)
+    } catch (e) {
+        console.log('Error while text message', e.message);
+    }
+})
 bot.command('start', async (ctx) => {
     await ctx.reply(JSON.stringify(ctx.message.voice));
 });
